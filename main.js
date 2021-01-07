c71b5a80610c5b7e4f2e353b7995cb92e71dbaab
Andrew Masih <armasih@outlook.com>
2021-01-07 18:57:00 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+var names = ['Frank', 'Anna', 'Bob', 'Sarah'];
+
+for (i =0; i < names.length; i++) {
+
+	console.log("i = " + i);
+	console.log("so:");
+	console.log("names[i]");
+	console.log("becomes:");
+	console.log("names["+i+"]");
+	console.log(names[i]);
+	console.log("*****");
+	console.log(" ");
+
+	
+
+};
+
+// structure of for loops:
+// for (i=0; i < ...; i++ ) {
+//     //  The code to execute for each iteration
+// }``
